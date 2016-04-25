3106f10433ea201a579a23d218d6e76a1f0fd49d
wasteland540 <m_m_elz@web.de>
2016-04-25 14:00:29 +0200

    Init Commit

---
+++ ajax/getTempsByDate.php
@@ -0,0 +1,20 @@
+<?php 
+require_once '../includes/db.php'; // The mysql database connection script
+if(isset($_GET['from']) && isset($_GET['to'])){
+$from = $_GET['from'];
+$to = $_GET['to'];
+
+$query="SELECT TempId, Date, Temp FROM office_temp WHERE Date BETWEEN '$from' AND '$to'";
+$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+$arr = array();
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$arr[] = $row;	
+	}
+}
+
+# JSON-encode the response
+echo $json_response = json_encode($arr);
+}
+?>
---
+++ app/app.js
@@ -0,0 +1,82 @@
+//Define an angular module for our app
+var app = angular.module('myApp', ['googlechart', 'ngMaterial']);
+
+app.controller('tempController', function($scope, $http, $timeout) {
+      
+  function getMonday(d) {
+    d = new Date(d);
+    var day = d.getDay(),
+        diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+    return new Date(d.setDate(diff));
+  }
+        
+  //init date range
+  $scope.from = getMonday(new Date());
+  $scope.to = new Date($scope.from.getFullYear(), $scope.from.getMonth(), $scope.from.getDate()+7);
+  
+  //start init angular-google-chart
+  $scope.myChartObject = {};
+  $scope.myChartObject.type = "AnnotationChart";
+
+  $scope.myChartObject.data = {  
+    "cols": [
+    {id: "dates", label: "Dates", type: "date"},
+    {id: "temp-data", label: "Temperature", type: "number"}
+  ]};
+ 
+  $scope.myChartObject.options = {
+    displayAnnotations: false
+  };
+  //end init angular-google-chart
+    
+  initTemps(); // Load all available temps in init date range 
+  
+  function initTemps(){  
+    getTempsInRange();
+  };
+  
+  //function for query btn
+  $scope.getTempsDateRange = function () {
+      getTempsInRange();
+  };
+  
+  function getTempsInRange(){
+        $http.post("ajax/getTempsByDate.php?from="+formatDate($scope.from) +"&to="+formatDate($scope.to)).success(function(data){           
+        var temps = data;
+        $scope.myChartObject.data.rows = createChartRows(temps);          
+    })
+    .error(function (error) {
+        console.log(error);
+    });
+  }
+  
+  function createChartRows(temps){
+     var rows = [];
+        
+     temps.forEach(function (entry) {            
+        rows.push({
+            c: [
+                {v: fromMySqlDate(entry.Date)},
+                {v: parseFloat(entry.Temp)}
+            ]
+        });   
+     });
+        
+     return rows;
+  }
+  
+  function formatDate(date){      
+      return date.getFullYear() +"-"+(date.getMonth()+1)+"-"+date.getDate();
+  }
+  
+  function fromMySqlDate(dateString){
+      // Split timestamp into [ Y, M, D, h, m, s ]
+    var t = dateString.split(/[- :]/);
+
+    // Apply each element to the Date function
+    var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+    
+    return d;
+  }  
+
+});
